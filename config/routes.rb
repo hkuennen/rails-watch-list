a852ae2042e75e3c84a2efd91cf0d33e89844741
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-11-18 15:42:49 +0100

    watch list done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "lists#index"
+  # root to: 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+
+  # get 'new', to: 'lists#new', as: :lists
+  # post 'lists', to: 'lists#create'
+  # get ':list_id', to: 'bookmarks#show', as: :list
+  # delete 'new', to: 'bookmarks#destroy'
+  # get ':list_id/:bookmark_id', to: 'movies#show' 
+
 end
